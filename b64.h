dbc8b17dba47053b45c6889feba8e3852dba62c6
ExSlam <114887800+ExSlam@users.noreply.github.com>
2024-03-17 23:00:21 +0000

    Add base64EncodeWithPaddingByLine function
    Store the position of newline characters for each line and add a padding before the newline if necessary.

---
+++ b64.h
@@ -0,0 +1,25 @@
+// This file is part of Notepad++ plugin MIME Tools project
+// Copyright (C)2023 Don HO <don.h@free.fr>
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// at your option any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+#pragma once
+
+#include <windows.h>
+#include <string>
+
+int base64Encode(char *resultString, const char *asciiString, size_t asciiStringLength, size_t wrapLength, bool padFlag, bool byLineFlag);
+int base64Decode(char *resultString, const char *encodedString, size_t encodedStringLength, bool strictFlag, bool whitespaceReset);
+int base64EncodeWithPaddingByLine(std::string& resultString, const char* asciiString, size_t asciiStringLength);
